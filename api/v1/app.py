faca9cd478996ce1ee9560e23efcf75ad2aff529
hedi <hedihamida98@gmail.com>
2022-12-07 22:39:13 +0100

    strict slashes

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,4 @@ def teardown():
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", default='0.0.0.0')
     port = getenv("HBNB_API_PORT", default='5000')
-
-    
     app.run(host=host, port=port, threaded=True)
