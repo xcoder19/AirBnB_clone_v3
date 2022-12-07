6f4c9e19aba089c4cef0a927c2e10479a6743f8c
hedi <hedihamida98@gmail.com>
2022-12-07 21:50:02 +0100

    doc fixing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ index """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/status")
 def status():
-    return '{"status": "OK"}'
-
+    return jsonify(status="OK")
