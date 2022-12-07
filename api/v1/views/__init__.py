9a8ddc0533626a61112083fd288ddb96943c2fd4
hedi <hedihamida98@gmail.com>
2022-12-07 18:54:42 +0100

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ init """
 from flask import Blueprint
-from api.v1.views.index import app_views
+from api.v1.views.index import *
 
 app_views = Blueprint(url_prefix="/api/v1")
