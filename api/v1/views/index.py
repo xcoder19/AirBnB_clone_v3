19c2cab4cbede39c7aaa236354458785dd10031a
hedi <hedihamida98@gmail.com>
2022-12-08 02:48:54 +0100

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,17 +5,20 @@ from flask import Flask, jsonify
 from models import storage
 
 classes = {
-"amenities": "Amenity",
-"cities": "City",
-"places": "Place",
-"reviews": "Review",
-"states": "State",
-"users": "User"
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
 }
+
+
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status """
     return jsonify(status="OK")
+
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
