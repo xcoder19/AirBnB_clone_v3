6c02564db27655c81e1fa71a39cd90d8e31c733c
hedi <hedihamida98@gmail.com>
2022-12-08 01:42:33 +0100

    count method fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,14 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 
+classes = {
+"amenities": "Amenity",
+"cities": "City",
+"places": "Place",
+"reviews": "Review",
+"states": "State",
+"users": "User"
+}
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status """
@@ -12,4 +20,7 @@ def status():
 @app_views.route("/stats", strict_slashes=False)
 def stats():
     """stats"""
-    return jsonify(storage.count())
+    dict = {}
+    for k, v in classes.items():
+        dict[k] = storage.count(v)
+    return jsonify(dict)
