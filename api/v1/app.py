9ccbb5aa549e1196e2e6548977473f894b6f6544
hedi <hedihamida98@gmail.com>
2022-12-08 02:02:37 +0100

    status code 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,7 +21,7 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def not_found(e):
-    return jsonify(error="Not found")
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
