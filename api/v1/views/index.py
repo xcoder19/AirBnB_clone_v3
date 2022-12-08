6cba8257aafac21659e8b9046cfd42062b8d0d5b
hedi <hedihamida98@gmail.com>
2022-12-08 01:34:43 +0100

    stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,14 @@
 """ index """
 from api.v1.views import app_views
 from flask import Flask, jsonify
-
+from models import storage
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status """
     return jsonify(status="OK")
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """stats"""
+    return jsonify(storage.count())
