bdd2708c8ab63d7ce10b02f83ef21e12ed86204e
xcoder19 <hedihamida98@gmail.com>
2022-12-08 01:21:51 +0000

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": '0.0.0.0'}})
+CORS(app, resources={r"/*": {"origins": '0.0.0.0'}})
 app.register_blueprint(app_views)
 
 
