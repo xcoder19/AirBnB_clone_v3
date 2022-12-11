98214e4e8b7b1feea54e1671b5356937bb2fd18d
hedi <hedihamida98@gmail.com>
2022-12-11 21:35:09 +0100

    amenities route

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,8 +5,7 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.state import City
-
-
+from models.amenity import Amenity
 @app_views.route('/amenities',
                  strict_slashes=False,
                  methods=['GET'])
